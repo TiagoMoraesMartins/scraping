7cc760bb2fcb424f8751f11a6b95fae401e9b805
Tiago MM <tiago.tmartins@gmail.com>
2023-09-11 16:57:01 -0300

    Estrutura de pastas

--- Scraping/forms.py
+++ Scraping/forms.py
@@ -1,7 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 class ScrapingForm(FlaskForm):
-    url = StringField(label='*Url:')
+    url = StringField(label='*Url:', validators=[DataRequired])
     submit = SubmitField(label='Enviar')
     
